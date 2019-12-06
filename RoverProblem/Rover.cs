7a0bbfdc0d0da320caaf24a8fe8226c2b2cb9a71
Neelesh Rajpurohit <nrajpurohit@tavisca.com>
2019-12-06 12:22:50 +0530

    basic solution setup and model created

---
+++ RoverProblem/Rover.cs
@@ -0,0 +1,20 @@
+﻿namespace RoverProblem
+{
+    class Rover
+    {
+        private RoverPosition _roverPosition;
+        public Rover(int xCordinate, int yCoordinate, char direction)
+        {
+            _roverPosition.initialize(xCordinate, yCoordinate, direction);
+        }
+        public void RotateRover(char RotateDirection)
+        {
+
+        }
+
+        public void MoveRover()
+        {
+
+        }
+    }
+}
---
+++ RoverProblem/RoverPosition.cs
@@ -0,0 +1,46 @@
+﻿namespace RoverProblem
+{
+    class RoverPosition
+    {
+        private int _x;
+        private int _y;
+        private char _direction;
+
+        public int GetXCord()
+        {
+            return _x;
+        }
+
+        public int GetYCord()
+        {
+            return _y;
+        }
+
+        public char GetDirection()
+        {
+            return _direction;
+        }
+
+        public void initialize(int xCoordinateValue, int yCoordinateValue, char directionValue)
+        {
+            this._x = xCoordinateValue;
+            this._y = yCoordinateValue;
+            this._direction = directionValue;
+        }
+
+        public void SetXCord(int newXCoordinateValue)
+        {
+            this._x = newXCoordinateValue;
+        }
+        public void SetYCord(int newYCoordinateValue)
+        {
+            this._y = newYCoordinateValue;
+        }
+
+        public void SetDirection(char newDirection)
+        {
+            this._direction = newDirection;
+        }
+
+    }
+}
