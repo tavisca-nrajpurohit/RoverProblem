024765714d557b5530b4289451835e24966ffc75
Neelesh Rajpurohit <nrajpurohit@tavisca.com>
2019-12-06 12:30:23 +0530

    added rotaion and movement functionality to rover

--- RoverProblem/Rover.cs
+++ RoverProblem/Rover.cs
@@ -7,14 +7,74 @@
         {
             _roverPosition.initialize(xCordinate, yCoordinate, direction);
         }
-        public void RotateRover(char RotateDirection)
+        public void RotateRover(char rotateDirection)
         {
-
+            switch (_roverPosition.GetDirection())
+            {
+                case 'N':
+                    if (rotateDirection == 'L')
+                    {
+                        _roverPosition.SetDirection('W');
+                    }
+                    else if (rotateDirection == 'R')
+                    {
+                        _roverPosition.SetDirection('E');
+                    }
+                    break;
+                case 'S':
+                    if (rotateDirection == 'L')
+                    {
+                        _roverPosition.SetDirection('E');
+                    }
+                    else if (rotateDirection == 'R')
+                    {
+                        _roverPosition.SetDirection('W');
+                    }
+                    break;
+                case 'E':
+                    if (rotateDirection == 'L')
+                    {
+                        _roverPosition.SetDirection('N');
+                    }
+                    else if (rotateDirection == 'R')
+                    {
+                        _roverPosition.SetDirection('S');
+                    }
+                    break;
+                case 'W':
+                    if (rotateDirection == 'L')
+                    {
+                        _roverPosition.SetDirection('S');
+                    }
+                    else if (rotateDirection == 'R')
+                    {
+                        _roverPosition.SetDirection('N');
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
 
         public void MoveRover()
         {
-
+            switch (_roverPosition.GetDirection())
+            {
+                case 'N':
+                    _roverPosition.SetYCord(_roverPosition.GetYCord() + 1);
+                    break;
+                case 'S':
+                    _roverPosition.SetYCord(_roverPosition.GetYCord() - 1);
+                    break;
+                case 'E':
+                    _roverPosition.SetXCord(_roverPosition.GetXCord() + 1);
+                    break;
+                case 'W':
+                    _roverPosition.SetXCord(_roverPosition.GetXCord() - 1);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
